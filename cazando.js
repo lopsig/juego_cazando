0cadb70794b60dbef237ab2a921b555806e4cdb4
lopsig <jonathan.ls.trabajo@gmail.com>
2026-04-12 09:10:19 -0500

    add limpiarCanva function and moverIzquierda function

--- cazando.js
+++ cazando.js
@@ -8,12 +8,12 @@ let comidaX = 0;
 let comidaY = 0;
 
 //CONSTANTES
-const ALTO_GATO = 200;
-const ANCHO_GATO = 300;
-const COLOR_GATO = "red";
-const ALTO_COMIDA = 50;
-const ANCHO_COMIDA = 50;
-const COLOR_COMIDA = "black"
+const ALTO_GATO = 50;
+const ANCHO_GATO = 75;
+const COLOR_GATO = "black";
+const ALTO_COMIDA = 20;
+const ANCHO_COMIDA = 20;
+const COLOR_COMIDA = "red"
 
 
 // FUNCIONES
@@ -26,10 +26,10 @@ const graficarComida = () => {
 }
 
 const iniciarJuego = () => {
-  gatoX = 100;
-  gatoY = 150;
-  comidaX = 450;
-  comidaY = 450;
+  gatoX = canvas.width/2 - ANCHO_GATO/2;
+  gatoY = canvas.height/2 - ALTO_GATO/2;
+  comidaX = canvas.width - ANCHO_COMIDA;
+  comidaY = canvas.height - ALTO_COMIDA;
   graficarGato();
   graficarComida();
 }
@@ -40,5 +40,15 @@ const graficarRectagulo = (x, y, ancho, alto, color) => {
   
 }
 
+const limpiarCanva = () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+};
 
+const moverIzquierda = () => {
+  gatoX = gatoX - 10;
+  limpiarCanva();
+  graficarGato();
+  graficarComida();
+
+}
 
